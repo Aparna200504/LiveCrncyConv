3f0b573ce0ddf429478f971ca94f7469b052c6f9
Aparna Prasad <142242410+Aparna200504@users.noreply.github.com>
2024-06-12 21:31:55 +0530

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,27 @@
+function convert() {
+    event.preventDefault();
+    let amt = document.getElementById("amt");
+    let msg = document.getElementById("msg");
+
+    if (amt.value === "") {
+        alert("You did not enter a amount");
+        msg.innerHTML = "";
+        amt.focus();
+        return;
+    }
+
+    let a = parseFloat(amt.value);
+    let url = "https://api.exchangerate-api.com/v4/latest/USD";
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        let dollar = data.rates.INR;
+        let rupees = dollar * a;
+        let ans = "\u20B9" + rupees.toFixed(2);
+        msg.innerHTML = ans;
+    })
+    .catch(err => {
+        alert("Issue: " + err);
+    });
+}
